26ca2c5110b13dae70fb089a8034c2ee943b73b1
tmannazy <e.anene@hotmail.com>
2021-08-30 23:06:07 +0100

    Display menu buttons on homepage

--- src/menuButtons.js
+++ src/menuButtons.js
@@ -1,12 +1,18 @@
-const title = () => {
-    // const div = document.createElement('div');
-    const titleLabel = document.createElement('label');
-    const titleInput = document.createElement('input');
-    titleLabel.setAttribute('for', 'title');
-    titleLabel.textContent = 'Title';
-    titleInput.setAttribute('type', 'text');
-    // div.append(titleLabel, titleInput);
-    // return { titleLabel, titleInput };
+const menu = () => {
+    const menuItems = ['home', 'today', 'week', 'project'];
+    const div = document.createElement('nav');
+    const list = document.createElement('ul');
+
+    const loopMenuItems = menuItems.forEach(element => {
+        const listItem = document.createElement('li');
+        listItem.textContent = element;
+        listItem.setAttribute('class', element);
+        list.appendChild(listItem);
+    });
+    // titleLabel.textContent = 'Title';
+    // titleInput.setAttribute('type', 'text');
+    div.append(list);
+    return div;
 }
 
-export { title };
+export { menu };
