e9c7926b5f01cd65acf559067ce1e98117e5f621
tmannazy <e.anene@hotmail.com>
2021-09-01 15:49:47 +0100

    Create modules to generate user's todo items

---
+++ src/todoItemsObj.js
@@ -0,0 +1,11 @@
+const TodoFunc = (title, description, notes, priority, date, project) => {
+    return {
+        title, description, notes, priority, date, project
+    };
+}
+
+const myTodo = [{
+
+}]
+
+export { myTodo, TodoFunc };
