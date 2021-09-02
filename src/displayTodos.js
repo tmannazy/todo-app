3be8cccc3ce3c8be9ffa02b8a7af022bd7abd72b
tmannazy <e.anene@hotmail.com>
2021-09-02 12:16:07 +0100

    Loop through myTodo array to display todos

--- src/displayTodos.js
+++ src/displayTodos.js
@@ -1,6 +1,14 @@
+import { myTodo } from "./todoItemsObj";
+
 const displayTodos = () => {
     const div = document.createElement('div');
     div.setAttribute('class', 'displayTodos');
+    const showMyTodo = myTodo;
+    showMyTodo.forEach((todo) => {
+        for (let showTodo in todo) {
+            console.log(todo[showTodo]);
+        }
+    });
     return div;
 }
 
