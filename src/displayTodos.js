c86b5ff3b60cee3f3c72c81862d9d9f6f178f618
tmannazy <e.anene@hotmail.com>
2021-09-03 23:23:18 +0100

    Create task container to hold the todo items

--- src/displayTodos.js
+++ src/displayTodos.js
@@ -5,8 +5,30 @@ const displayTodos = () => {
     div.setAttribute('class', 'displayTodos');
     const showMyTodo = myTodo;
     showMyTodo.forEach((todo) => {
+        const taskContainer = document.createElement('div');
         for (let showTodo in todo) {
             console.log(todo[showTodo]);
+            const taskItem = document.createElement('div');
+            const taskItemLabel = document.createElement('label');
+            const taskCheckbox = document.createElement('input');
+            taskItem.setAttribute('class', 'items');
+            taskCheckbox.setAttribute('type', 'checkbox');
+            switch (showTodo) {
+                case 'title':
+                    taskItem.setAttribute('class', `task${showTodo}`);
+                    taskItem.textContent = `${todo[showTodo]}`;
+                    taskItemLabel.textContent = showTodo;
+                    break;
+                case 'description':
+                    taskItem.setAttribute('class', `task${showTodo}`);
+                    taskItem.textContent = `${todo[showTodo]}`;
+                    taskItemLabel.textContent = showTodo;
+                    break;
+                default:
+                    break;
+            }
+            taskContainer.append(taskCheckbox, taskItemLabel, taskItem);
+            div.appendChild(taskContainer);
         }
     });
     return div;
