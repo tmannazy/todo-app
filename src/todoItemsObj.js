2ac74b23d5ecf0dd86821459e96ea6b43dea6ee9
tmannazy <e.anene@hotmail.com>
2021-09-17 13:39:14 +0100

    Add a default todo item on page load

--- src/todoItemsObj.js
+++ src/todoItemsObj.js
@@ -4,6 +4,13 @@ const TodoFunc = (title, description, notes, priority, date, project) => {
     };
 }
 
-const myTodo = [{}]
+const myTodo = [{
+    title: 'Practice Coding Exercise',
+    description: 'Keep the Acronyms in mind - \'ABL, ABB, CCC\' in mind daily to draw inspiration',
+    notes: 'ABL stands for Always Be Learning. ABB stands for Always Be Building. CCC stands for Code Code Code. These acronyms were invented by Danny Thompson as one of the major attributes to become a better to be a programmer',
+    priority: 'High',
+    date: new Date(),
+    project: 'Coding'
+}]
 
 export { myTodo, TodoFunc };
