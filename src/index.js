8b8e5118815434eb892195199b67ce196572c200
tmannazy <e.anene@hotmail.com>
2021-09-20 21:34:11 +0100

    Fix bug to remove selected project from side menu items

--- src/index.js
+++ src/index.js
@@ -120,18 +120,14 @@ const displayNewProjectItem = () => {
 };
 
 const delAddedProjItem = () => {
-    const delProjItemBtn = document.querySelector('.project-list');
-    let children = delProjItemBtn.children;
-    for (let i = 4; i < children.length; i++) {
-        delProjItemBtn.addEventListener('click', event => {
-            // if (children[i] > children[4]) {
-            if (event.target[i] !== event.currentTarget && event.target[i] !== addNewProject) {
+    const delProjItemBtn = document.querySelector('.project-list')
+        .addEventListener('click', event => {
+            if (event.target.className === 'del-project' && event.target !== event.currentTarget && event.target !== addNewProject) {
                 const parentNode = event.target.parentNode;
                 parentNode.remove();
-            } event.stopPropagation();
+                event.stopPropagation();
+            }
         });
-        // }
-    }
 }
 
 const closeNewProject = () => {
