bc5900d40bd5bc951a7de107192a336a9cb5af96
tmannazy <e.anene@hotmail.com>
2021-09-27 00:07:28 +0100

    Show date in formatted ways

--- src/todoItemsObj.js
+++ src/todoItemsObj.js
@@ -13,7 +13,7 @@ const myTodoList = [{
     description: 'Keep the Acronyms in mind - \'ABL, ABB, CCC\' in mind daily to draw inspiration',
     notes: 'ABL stands for Always Be Learning. ABB stands for Always Be Building. CCC stands for Code Code Code. These acronyms were invented by Danny Thompson as one of the major attributes to become a better to be a programmer',
     priority: 'High',
-    date: format(new Date(), 'MM/dd/yyyy', { weekStartsOn: 5, locale }),
+    date: startOfWeek(new Date(), { weekStartsOn: 5, locale }),
     project: 'Coding'
 }]
 
