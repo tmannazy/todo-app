59768090d34f8c30cd39f1527044d339964ff8b6
tmannazy <e.anene@hotmail.com>
2021-09-30 15:36:17 +0100

    Edit date key/value pair of myTodoList object

--- src/todoItemsObj.js
+++ src/todoItemsObj.js
@@ -13,20 +13,9 @@ const myTodoList = [{
     description: 'Keep the Acronyms in mind - \'ABL, ABB, CCC\' in mind daily to draw inspiration',
     notes: 'ABL stands for Always Be Learning. ABB stands for Always Be Building. CCC stands for Code Code Code. These acronyms were invented by Danny Thompson as one of the major attributes to become a better to be a programmer',
     priority: 'High',
-    date: format(new Date(), 'dd.MM.yyyy'),
+    date: new Date(),
     project: 'Coding'
 }]
 
 
-const getTodosByProjectName = projectName => {
-    const loopTodoList = myTodoList.filter(todoItem => {
-        const checkProjectName = Object.values(todoItem).forEach(item => {
-            const smallCase = item.toLowerCase();
-            const div = document.createElement('div');
-            if (smallCase === projectName) {
-                div.appendChild(displayTodos(todoItem));
-            }
-        });
-    });
-}
-export { myTodoList, TodoFactoryFunction, getTodosByProjectName };
+export { myTodoList, TodoFactoryFunction };
