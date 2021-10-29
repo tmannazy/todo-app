e5237fa23458508d5d20e7d6b48fd420ace9f8bd
tmannazy <e.anene@hotmail.com>
2021-10-29 09:25:23 +0100

    Refactor code using formatter

--- src/todoItemsObj.js
+++ src/todoItemsObj.js
@@ -1,42 +1,56 @@
-import { format } from 'date-fns';
+import { format } from "date-fns";
 
+const TodoFactoryFunction = (
+  title,
+  description,
+  notes,
+  priority,
+  date,
+  project
+) => {
+  return {
+    title,
+    description,
+    notes,
+    priority,
+    date,
+    project,
+  };
+};
 
+const myTodoList = [
+  {
+    title: "Practice Coding Exercise",
+    description:
+      "Keep the Acronyms in mind - 'ABL, ABB, CCC' in mind daily to draw inspiration",
+    notes:
+      "ABL stands for Always Be Learning. ABB stands for Always Be Building. CCC stands for Code Code Code. These acronyms were invented by Danny Thompson as one of the major attributes to become a better to be a programmer",
+    priority: "High",
+    date: format(new Date(), "dd.MM.yyyy"),
+    project: "Coding",
+  },
+];
 
-const TodoFactoryFunction = (title, description, notes, priority, date, project) => {
-    return {
-        title, description, notes, priority, date, project
-    };
-}
-
-const myTodoList = [{
-    title: 'Practice Coding Exercise',
-    description: 'Keep the Acronyms in mind - \'ABL, ABB, CCC\' in mind daily to draw inspiration',
-    notes: 'ABL stands for Always Be Learning. ABB stands for Always Be Building. CCC stands for Code Code Code. These acronyms were invented by Danny Thompson as one of the major attributes to become a better to be a programmer',
-    priority: 'High',
-    date: format(new Date(), 'dd.MM.yyyy'),
-    project: 'Coding'
-}];
-
-
-
-const storeTodoItemsToLocalStorage = newTodo => {
-    function checkForDefaultTodoItem() {
-        const defaultTodoItem = getStoredTodoItems.some(itemToCheck => itemToCheck.title === 'Practice Coding Exercise');
-        return defaultTodoItem;
-    }
-    const getStoredTodoItems = JSON.parse(localStorage.getItem('todoLists') || '[]');
-    if (!(newTodo === undefined || newTodo === null))
-        getStoredTodoItems.push(newTodo);
-    else if (typeof newTodo === 'number')
-        return true;
-    else if (newTodo === undefined && !checkForDefaultTodoItem())
-        getStoredTodoItems.push(myTodoList[0]);
-    const stringifyTodoItems = localStorage.setItem('todoLists', JSON.stringify(getStoredTodoItems));
-    return getStoredTodoItems;
-}
-
-
-
-
+const storeTodoItemsToLocalStorage = (newTodo) => {
+  function checkForDefaultTodoItem() {
+    const defaultTodoItem = getStoredTodoItems.some(
+      (itemToCheck) => itemToCheck.title === "Practice Coding Exercise"
+    );
+    return defaultTodoItem;
+  }
+  const getStoredTodoItems = JSON.parse(
+    localStorage.getItem("todoLists") || "[]"
+  );
+  if (!(newTodo === undefined || newTodo === null))
+    getStoredTodoItems.push(newTodo);
+  else if (typeof newTodo === "number") return true;
+  else if (newTodo === undefined && !checkForDefaultTodoItem())
+    getStoredTodoItems.push(myTodoList[0]);
+  const stringifyTodoItems = localStorage.setItem(
+    "todoLists",
+    JSON.stringify(getStoredTodoItems)
+  );
+  return getStoredTodoItems;
+};
 
 export { myTodoList, TodoFactoryFunction, storeTodoItemsToLocalStorage };
